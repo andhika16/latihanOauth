98e4e6e3ef3e48931c8bab4f3d18f1873e616673
andhika16 <moh.andhika16@gmail.com>
2020-12-14 20:15:22 +0700

    aplikasi selesai perlu refactor

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const {ensureAuth,ensureGuest} = require('../config/auth');
 const User = require('../model/facebook-user');
+
 
 // login page
 router.get('/login', (req, res) => {
@@ -17,33 +17,31 @@ router.get('/about', (req, res) => {
     res.render('about')
 })
 
-// github page
-router.get('/github_user', async (req, res) => {
+// about page
+router.get('/profile' ,async (req, res) => {
+    
     const user = await User.findOne({
-        // bagaimana cara agar session tidak tambah didatabase dan access_token tetap
+// bagaimana cara agar session tidak tambah didatabase dan access_token tetap
         githubId:req.session.githubId
     })
-    if(user){
-        res.render('github_user',{
-            // untuk sementara load secara manual pada json data
+// user github ambil data dari database untuk user google ambil data req.user 
+// kondisi dibawah sebagai pemisah antara user github dan google
+    if(user) {
+        res.render('profile', {
             displayName:user.displayName,
             firstName:user.firstName,
             image:user.image
         })
     } else {
-        res.send('session expired')
+        res.render('profile',{
+            // terjadi error pada saat login user github pada render req.user
+            displayName:req.user.displayName,
+            firstName:req.user.firstName,
+            image:req.user.image
+        })
     }
+
+   
 })
-
-
-// about page
-router.get('/profile', (req, res) => {
-    res.render('profile',{
-        displayName:req.user.displayName,
-        firstName:req.user.firstName,
-        image:req.user.image
-    })
-})
-
 
 module.exports = router;
